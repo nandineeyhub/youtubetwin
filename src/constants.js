12a917891bdd79ab5b7b24f1a8cd03ce71d9d6a0
Nandini <nandini2114001@akgec.ac.in>
2023-04-25 21:32:43 +0530

    new

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
+const API_KEY = "AIzaSyCIzpijohe4Zh151_0PPMyriA5hbNheucg"
 
-
-export const VID_LIST = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=AIzaSyCIzpijohe4Zh151_0PPMyriA5hbNheucg"
+export const VID_LIST = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + API_KEY
